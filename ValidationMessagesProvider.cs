acc808b6a5e69bd9a43db00196f95d8302939c7a
Lucasus <lukasz.wiatrak@gmail.com>
2013-02-16 19:07:04 +0100

    Added ValidationMessagesProvider

---
+++ ValidationMessagesProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace LucValidation
+{
+    public abstract class ValidationMessagesProvider
+    {
+        public static ValidationMessagesProvider Current { get; set; }
+
+        public abstract string InvalidFormatInt { get; }
+        public abstract string InvalidFormatDateTime { get; }
+        public abstract string InvalidFormatBool { get; }
+
+        public abstract string GetText(string key);
+    }
+}
